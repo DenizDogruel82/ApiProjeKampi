46b570fd9eca89e48fc20092b3cbeee026ce590f
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-09-24 09:24:32 +0300

    category api testi yapıldı

---
+++ ApiProjeKampi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,58 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category) { 
+          _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Ekleme başarılı");
+
+        
+        }
+        [HttpGet]
+        public IActionResult GetCategories() { 
+        
+           var value = _context.Categories.ToList();
+            return Ok(value);
+
+        }
+        [HttpDelete]
+        public IActionResult DeleteCategories(int id) {
+            var value = _context.Categories.Find(id);
+           _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Silme işlemi Başarılı");
+            
+
+
+        }
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id) { 
+        
+        var value= _context.Categories.Find(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category) { 
+        
+        _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Guncelleme işlemi başarılı");
+        
+        }
+    }
+}
