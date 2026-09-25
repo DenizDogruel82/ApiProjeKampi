b849abbf98562ba40f598176710f83ec484c1ada
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-09-25 08:32:22 +0300

    message dtoları eklendi

---
+++ ApiProjeKampi.WebApi/Dtos/MessageDtos/CreateMessageDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjeKampi.WebApi.Dtos.MessageDtos
+{
+    public class CreateMessageDto
+    {
+        public string NameSurname { get; set; }
+        public string Subject { get; set; }
+        public string MessageDetails { get; set; }
+        public DateTime Senddate { get; set; }
+        public bool isRead { get; set; }
+    }
+}
---
+++ ApiProjeKampi.WebApi/Dtos/MessageDtos/GetByIdMessageDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProjeKampi.WebApi.Dtos.MessageDtos
+{
+    public class GetByIdMessageDto
+    {
+        public int MessageId { get; set; }
+        public string NameSurname { get; set; }
+        public string Subject { get; set; }
+        public string MessageDetails { get; set; }
+        public DateTime Senddate { get; set; }
+        public bool isRead { get; set; }
+    }
+}
