c3198fa1370e98341d17f51f7dd4eacafb899ae1
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-09-25 08:40:08 +0300

    General mapping classıile automapper eşlemeleri yapıldı

---
+++ ApiProjeKampi.WebApi/GeneralMaping/GeneralMapping.cs
@@ -0,0 +1,22 @@
+﻿using ApiProjeKampi.WebApi.Dtos.FeatureDtos;
+using ApiProjeKampi.WebApi.Dtos.MessageDtos;
+using ApiProjeKampi.WebApi.Entities;
+using AutoMapper;
+
+namespace ApiProjeKampi.WebApi.GeneralMaping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature,CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature,UpdateFeatureDto>().ReverseMap();
+            CreateMap<Feature,ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature,GetByIdFeatureDto>().ReverseMap();
+            CreateMap<Message, CreateMessageDto>().ReverseMap();
+            CreateMap<Message, UpdateMessageDto>().ReverseMap();
+            CreateMap<Message, ResultMessageDto>().ReverseMap();
+            CreateMap<Message, GetByIdMessageDto>().ReverseMap();
+        }
+    }
+}
